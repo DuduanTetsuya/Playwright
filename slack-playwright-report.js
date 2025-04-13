56451d01dd59bc9f474d9e2ae0e4bc8ebe6b7b5a
DESKTOP-REOCIRT\duanestra <duanestra@gmail.com>
2025-04-13 16:58:05 +0700

    no message

--- slack-playwright-report.js
+++ slack-playwright-report.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const axios = require("axios");
 
-const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"; // Ganti dengan webhook kamu
+//const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"; // Ganti dengan webhook kamu
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
 // Tunggu file JSON tersedia
 const waitForFile = (filePath, timeout = 5000) => {
